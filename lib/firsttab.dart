fc9f83a1bba57503f89a917b80e84cad96063265
Loai Medhat Yousif <loaimedhat4@gmail.com>
2024-12-29 20:48:55 +0200

    Update

--- lib/firsttab.dart
+++ lib/firsttab.dart
@@ -23,37 +23,56 @@ class _FirsttabState extends State<Firsttab> {
 
   @override
   Widget build(BuildContext context) {
-    return MobileScanner(
-      controller: MobileScannerController(
-        detectionSpeed: DetectionSpeed.noDuplicates,
-        returnImage: true,
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Padding(
+        padding: EdgeInsets.only(bottom: 30),
+        child: Text(
+          "Scan Your QR Code",
+          style: TextStyle(fontSize: 24),
+        ),
       ),
-      onDetect: (capture) {
-        final List<Barcode> barcodes = capture.barcodes;
-        final Uint8List? image = capture.image;
-        for (final barcode in barcodes) {
-          final String? rawValue = barcode.rawValue;
-          if (rawValue != null) {
-            _copyToClipboard(rawValue);
-            if (mounted && image != null) {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text(
-                      rawValue,
-                    ),
-                    content: Image(
-                      image: MemoryImage(image),
-                    ),
-                  );
-                },
-              );
-            }
-            break;
-          }
-        }
-      },
-    );
+      SizedBox(
+        height: 300,
+        width: 300,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(width: 6, color: Colors.blue),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: MobileScanner(
+            controller: MobileScannerController(
+              detectionSpeed: DetectionSpeed.noDuplicates,
+              returnImage: true,
+            ),
+            onDetect: (capture) {
+              final List<Barcode> barcodes = capture.barcodes;
+              final Uint8List? image = capture.image;
+              for (final barcode in barcodes) {
+                final String? rawValue = barcode.rawValue;
+                if (rawValue != null) {
+                  _copyToClipboard(rawValue);
+                  if (mounted && image != null) {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text(
+                            rawValue,
+                          ),
+                          content: Image(
+                            image: MemoryImage(image),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                  break;
+                }
+              }
+            },
+          ),
+        ),
+      ),
+    ]);
   }
 }
